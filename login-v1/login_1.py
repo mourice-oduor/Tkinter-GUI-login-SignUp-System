0460a73be611a59fafa7162d273549990f4bd981
mourice-oduor <otienomaurice364@gmail.com>
2020-03-28 10:56:54 -0400

    updated login-v1

--- login-v1/login_1.py
+++ login-v1/login_1.py
@@ -1,9 +1,10 @@
 from tkinter import *
 import sqlite3
-from tkinter import messagebox
 
 window=Tk()
+window.title("Python: GUI Login Application")
 window.geometry("400x200")
+#window.configure(bg='#105e1a')
 
 def login():
 	def login_database():
@@ -15,21 +16,22 @@ def login():
 		print(row)
 		if row!=[]:
 			user_name=row[0][1]
-			l3.config(text="user name found with name: "+user_name)
+			l3.config(text="user name found with name:\n "+user_name,fg= "green" ,font = ("calibri", 18))
 		else:
 			l3.config(text="user not found ")
-
+       
 
 
 	window.destroy()
 	login_window=Tk()
-	login_window.geometry("400x250")
+	login_window.geometry("450x250")
 	l1=Label(login_window,text="email",font="times 20")
 	l1.grid(row=1,column=1)
 	l2=Label(login_window,text="password",font="times 20")
 	l2.grid(row=2,column=1)
 	l3=Label(login_window,font="times 20")
 	l3.grid(row=5,column=2)
+	print("Login session started")
 
 	email_text=StringVar()
 	e1=Entry(login_window,textvariable=email_text)
@@ -41,6 +43,8 @@ def login():
 
 	b1=Button(login_window,text="login",width=20,fg="#0ad135", bg="#120b4d",command=login_database)
 	b1.grid(row=4,column=2)
+	#b3 = Button(window, text='Back', command=Back)
+	#b3.grid(row=6,column=2)
 	login_window.mainloop()
 
 
@@ -56,6 +60,7 @@ def signup():
 		cur.execute("INSERT INTO test Values(Null,?,?,?)",(e1.get(),e2.get(),e3.get()))
 		l4=Label(signup_window,text="account created",font="times 15")
 		l4.grid(row=6,column=2)
+		Label(signup_window, text = "Registration Sucess", fg = "green" ,font = ("calibri", 14)).grid(row=8,column=2)
 		conn.commit()
 		conn.close()
 
@@ -86,21 +91,26 @@ def signup():
 
 	b1=Button(signup_window,text="login",width=18,fg="#0ad135", bg="#120b4d",command=signup_database)
 	b1.grid(row=4,column=2)
+	#b3 = Button(window, text='Back', command=Back)
+	#b3.grid(row=6,column=2)
+	
+
+# def Back():
+#     window.destroy()
+#     window.deiconify()
 
 
-
-
-
-
-
-l1=Label(window,text="Please Login or SignUp\n first in order to continue ",font="times 20")
+l1=Label(window,text="Please Login or SignUp\n first in order to continue ",fg="red",bg="white",font="times 20")
 l1.grid(row=1,column=2,columnspan=2)
 
-b1=Button(window,text="login",width=20,command=login)
+b1=Button(window,text="Login",width=20,command=login)
 b1.grid(row=2,column=3)
 
-b2=Button(window,text="signup",width=20,command=signup)
+b2=Button(window,text="Signup",width=20,command=signup)
 b2.grid(row=4,column=3)
 
 
 window.mainloop()
+
+if __name__ == '__main__':
+    window.mainloop()
